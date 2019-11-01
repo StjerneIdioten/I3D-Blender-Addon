618ca0d1e37ec37136d24a69c202948d0b1a67f1
Tallion_07 <opptrans@gmail.com>
2019-11-02 01:53:24 +1000

    Python3 shebang linter and comment fix

--- io_export_i3d/i3d_IOexport.py
+++ io_export_i3d/i3d_IOexport.py
@@ -1,25 +1,28 @@
-!#/usr/bin/python3
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
-#
-# Copyright 2004 (C) GIANTS Software GmbH, Confidential, All Rights Reserved.
+#!/usr/bin/env python3
 
-# TODO: IK, Animations, Skin Weights
+"""
+ ##### BEGIN GPL LICENSE BLOCK #####
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ ##### END GPL LICENSE BLOCK #####
+
+ Copyright 2004 (C) GIANTS Software GmbH, Confidential, All Rights Reserved.
+
+ TODO: IK, Animations, Skin Weights
+"""
 
 import bpy
 import bmesh
