9baf549af1603cc927d053a36ccade21916c4713
Tallion_07 <opptrans@gmail.com>
2019-11-02 01:31:52 +1000

    shebang correction for python3 linter

--- io_export_i3d/__init__.py
+++ io_export_i3d/__init__.py
@@ -1,26 +1,26 @@
-!#/usr/bin/python3
+#!/usr/bin/env python3
 
-# ##### BEGIN GPL LICENSE BLOCK #####
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-# ##### END GPL LICENSE BLOCK #####
+"""             ##### BEGIN GPL LICENSE BLOCK #####.
 
-# TODO: check current programming
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
 
-# <pep8-80 compliant>
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+                ##### END GPL LICENSE BLOCK #####
+
+TODO: check current programming
+
+<pep8-80 compliant>
+"""
 
 import bpy
 
