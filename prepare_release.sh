cb0fec3bcccbc94de46030dd44260f5d70c1238f
stjerneidioten <stjerneidioten@gmail.com>
2020-09-05 02:37:08 +0200

    ci(semantic-release): Properly handle dev version numbers
    Update the preparation script and python to handle dev version numbers
    
    Close #65

--- prepare_release.sh
+++ prepare_release.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# Replace version number
+sed -i "/^__version/c\__version__ = \"$1\"" $GITHUB_WORKSPACE/addon/test/__init__.py
 # Split the version tag into separate numbers
-IFS='.' read -ra VERSION <<< "$1"
-# Replace version number with new version number
+IFS='.-' read -ra VERSION <<< "$1"
+# Replace bl_info version number with new version number (This one can't contain a development tag if present)
 sed -ri -E "/version/s/[0-9]+, [0-9]+, [0-9]+/${VERSION[0]}, ${VERSION[1]}, ${VERSION[2]}/" $GITHUB_WORKSPACE/addon/i3dio/__init__.py
 # Zip it all into the new build
 sudo apt-get install -y zip
