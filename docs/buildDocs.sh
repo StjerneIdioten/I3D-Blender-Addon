01d32a004e8ea6785f626568874cd74cad6a6f2b
stjerneidioten <stjerneidioten@gmail.com>
2020-09-30 23:45:05 +0200

    ci(sphinx): Load requirements from requirements file

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -18,7 +18,9 @@ set -x
  
 apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme
- 
+
+pip3 install ../addon/requirements.txt
+
 #####################
 # DECLARE VARIABLES #
 #####################
