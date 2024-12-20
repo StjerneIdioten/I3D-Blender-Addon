45781134591b7a614d8f69e81d1d64b375674408
Kristian <57712777+NMC-TBone@users.noreply.github.com>
2024-12-20 13:48:31 -0700

    feat(i3d mapping): add i3d mapping support for bones (#233)
    Co-authored-by: LKAMinco <70859762+LKAMinco@users.noreply.github.com>

--- addon/i3dio/ui/object.py
+++ addon/i3dio/ui/object.py
@@ -848,6 +848,7 @@ class I3DMappingData(bpy.types.PropertyGroup):
         default=''
     )
 
+
 @register
 class I3D_IO_PT_mapping_attributes(Panel):
     bl_space_type = 'PROPERTIES'
@@ -864,7 +865,7 @@ class I3D_IO_PT_mapping_attributes(Panel):
         layout = self.layout
         layout.use_property_split = True
         layout.use_property_decorate = False
-        obj = bpy.context.active_object
+        obj = context.object
 
         row = layout.row()
         row.prop(obj.i3d_mapping, 'is_mapped')
@@ -872,12 +873,37 @@ class I3D_IO_PT_mapping_attributes(Panel):
         row.prop(obj.i3d_mapping, 'mapping_name')
 
 
+@register
+class I3D_IO_PT_mapping_bone_attributes(Panel):
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_label = "I3D Mapping"
+    bl_context = 'bone'
+
+    @classmethod
+    def poll(cls, context):
+        return context.bone or context.edit_bone
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+        layout.use_property_decorate = False
+        bone = context.bone or context.edit_bone
+
+        row = layout.row()
+        row.prop(bone.i3d_mapping, 'is_mapped')
+        row = layout.row()
+        row.prop(bone.i3d_mapping, 'mapping_name')
+
+
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
     bpy.types.Object.i3d_attributes = PointerProperty(type=I3DNodeObjectAttributes)
-    bpy.types.Object.i3d_merge_group_index = IntProperty(default = -1)
+    bpy.types.Object.i3d_merge_group_index = IntProperty(default=-1)
     bpy.types.Object.i3d_mapping = PointerProperty(type=I3DMappingData)
+    bpy.types.Bone.i3d_mapping = PointerProperty(type=I3DMappingData)
+    bpy.types.EditBone.i3d_mapping = PointerProperty(type=I3DMappingData)
     bpy.types.Object.i3d_reference_path = StringProperty(
         name="Reference Path",
         description="Put the path to the .i3d file you want to reference here",
@@ -886,10 +912,13 @@ def register():
     bpy.types.Scene.i3dio_merge_groups = CollectionProperty(type=I3DMergeGroup)
     load_post.append(handle_old_merge_groups)
 
+
 def unregister():
     load_post.remove(handle_old_merge_groups)
     del bpy.types.Scene.i3dio_merge_groups
     del bpy.types.Object.i3d_reference_path
+    del bpy.types.EditBone.i3d_mapping
+    del bpy.types.Bone.i3d_mapping
     del bpy.types.Object.i3d_mapping
     del bpy.types.Object.i3d_merge_group_index
     del bpy.types.Object.i3d_attributes
