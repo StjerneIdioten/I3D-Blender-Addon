f47dc3578ed2537bb08fa37a5283f902e731d56b
Kristian <57712777+NMC-TBone@users.noreply.github.com>
2025-01-04 14:57:32 -0700

    feat(exporter): add property to exclude objects and their children from export
    This feature can be used to keep e.g. objects for reference, that will be spawned by the game in the final model

--- addon/i3dio/exporter.py
+++ addon/i3dio/exporter.py
@@ -177,6 +177,11 @@ def _export(i3d: I3D, objects: List[BlenderObject], sort_alphabetical: bool = Tr
 
 
 def _add_object_to_i3d(i3d: I3D, obj: BlenderObject, parent: SceneGraphNode = None) -> None:
+    # Check if object should be excluded from export (including its children)
+    if obj.i3d_attributes.exclude_from_export:
+        logger.info(f"Skipping [{obj.name}] and its children. Excluded from export.")
+        return
+
     # Special handling of armature nodes, since they are sort of "extra" compared to how other programs like Maya
     # handles bones. So the option for turning them off is provided.
     _parent = parent
