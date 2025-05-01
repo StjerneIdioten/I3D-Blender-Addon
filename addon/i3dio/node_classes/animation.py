fe773545631d654adac8077f8e686bfead516d7c
Kristian <57712777+NMC-TBone@users.noreply.github.com>
2025-05-02 00:37:33 +0200

    restructure and add support for baking keyframes

--- addon/i3dio/node_classes/animation.py
+++ addon/i3dio/node_classes/animation.py
@@ -2,7 +2,6 @@ import logging
 import math
 import bpy
 from bpy_extras import anim_utils
-from dataclasses import dataclass
 
 from .node import SceneGraphNode
 from .skinned_mesh import SkinnedMeshBoneNode
@@ -20,14 +19,66 @@ class BaseAnimationExport:
                                                   {'object_name': type(self).__name__})
 
 
-class BaseAnimationNode(BaseAnimationExport):
-    def __init__(self, i3d: I3D, fps: float, node: SceneGraphNode | SkinnedMeshBoneNode):
+class Keyframe:
+    @staticmethod
+    def generate_keyframe(i3d: I3D,
+                          node: SceneGraphNode | SkinnedMeshBoneNode,
+                          is_bone: bool,
+                          frame: int,
+                          time_ms: float,
+                          export_translation: bool,
+                          export_rotation: bool,
+                          export_scale: bool) -> xml_i3d.Element:
+
+        i3d.depsgraph.scene.frame_set(frame)
+
+        local_matrix = node.blender_object.matrix_local.copy()
+        if is_bone:
+            if pose_bone := node.root_node.blender_object.pose.bones.get(node.blender_object.name):
+                local_matrix = pose_bone.matrix.copy()
+            if isinstance(node.parent, SkinnedMeshBoneNode):
+                # Bone with bone parent — use raw matrix
+                if parent_pose_bone := node.root_node.blender_object.pose.bones.get(node.parent.blender_object.name):
+                    local_matrix = parent_pose_bone.matrix.inverted_safe() @ local_matrix
+                conv_matrix = local_matrix
+            else:
+                conv_matrix = i3d.conversion_matrix @ local_matrix
+        else:
+            conv_matrix = i3d.conversion_matrix @ local_matrix @ i3d.conversion_matrix.inverted_safe()
+
+        translation, rotation, scale = conv_matrix.decompose()
+        rotation = rotation.to_euler('XYZ')
+        keyframe_element = xml_i3d.Element("Keyframe", {"time": f"{time_ms:.6g}"})
+        if export_translation:
+            keyframe_element.set("translation", "{0:.6g} {1:.6g} {2:.6g}".format(*translation))
+        if export_rotation:
+            keyframe_element.set("rotation", " ".join(f"{math.degrees(a):.6g}" for a in rotation))
+        if export_scale:
+            keyframe_element.set("scale", "{0:.6g} {1:.6g} {2:.6g}".format(*scale))
+
+        return keyframe_element
+
+
+class Keyframes(BaseAnimationExport):
+    def __init__(self,
+                 i3d: I3D,
+                 fps: float,
+                 node: SceneGraphNode | SkinnedMeshBoneNode,
+                 channelbag: bpy.types.ActionChannelbag,
+                 start_frame: int,
+                 end_frame: int):
         super().__init__(i3d, fps)
         self.node = node
         self.is_bone = isinstance(node, SkinnedMeshBoneNode)
-        self.parent_is_bone = isinstance(node.parent, SkinnedMeshBoneNode)
-        self.fcurves: list[bpy.types.FCurve] = []
-        self.should_bake: bool = False
+        self.channelbag = channelbag
+        self.start_frame = start_frame
+        self.end_frame = end_frame
+        self.fcurves = self._filter_fcurves(channelbag)
+        self.has_translation = any(fc.data_path.endswith("location") for fc in self.fcurves)
+        self.has_rotation = any(fc.data_path.endswith("rotation_euler") for fc in self.fcurves)
+        self.has_scale = any(fc.data_path.endswith("scale") for fc in self.fcurves)
+
+        self.xml_element = self._generate_keyframes()
 
     def _filter_fcurves(self, channelbag: bpy.types.ActionChannelbag) -> list[bpy.types.FCurve]:
         if self.is_bone:
@@ -35,87 +86,11 @@ class BaseAnimationNode(BaseAnimationExport):
             return [fc for fc in channelbag.fcurves if f'pose.bones["{bone_name}"]' in fc.data_path]
         return channelbag.fcurves
 
-
-@dataclass
-class KeyframeContext:
-    node: SceneGraphNode | SkinnedMeshBoneNode
-    is_bone: bool
-    frame: float
-    time_ms: float
-    export_translation: bool
-    export_rotation: bool
-    export_scale: bool
-    should_bake: bool = False
-
-
-class Keyframe(BaseAnimationExport):
-    def __init__(self, i3d: I3D, context: KeyframeContext):
-        super().__init__(i3d, i3d.depsgraph.scene.render.fps)
-        self.ctx = context
-        self._generate_keyframe()
-
-    def _generate_keyframe(self):
-        """Generates and returns the XML representation of the keyframe at a specific frame."""
-        """ if self.should_bake:
-            # If baking is needed, we need to sample the keyframe at the current frame
-            self.time_ms = ((self.frame - self.i3d.start_frame) / self.i3d.fps) * 1000 """
-
-        self.i3d.depsgraph.scene.frame_set(int(self.ctx.frame))
-
-        node = self.ctx.node
-
-        if self.ctx.is_bone:
-            if pose_bone := node.root_node.blender_object.pose.bones.get(node.blender_object.name):
-                local_matrix = pose_bone.matrix.copy()
-        else:
-            local_matrix = node.blender_object.matrix_local.copy()
-
-        if self.ctx.is_bone:
-            if isinstance(node.parent, SkinnedMeshBoneNode):
-                # Bone with bone parent — use raw matrix
-                if parent_pose_bone := node.root_node.blender_object.pose.bones.get(node.parent.blender_object.name):
-                    local_matrix = parent_pose_bone.matrix.inverted_safe() @ local_matrix
-                conv_matrix = local_matrix
-            else:
-                conv_matrix = self.i3d.conversion_matrix @ local_matrix
-        else:
-            conv_matrix = self.i3d.conversion_matrix @ local_matrix @ self.i3d.conversion_matrix.inverted_safe()
-
-        final_translation, final_rotation, final_scale = conv_matrix.decompose()
-        final_rotation = final_rotation.to_euler('XYZ')
-        self.xml_element = xml_i3d.Element("Keyframe", {"time": f"{self.ctx.time_ms:.6g}"})
-        if self.ctx.export_translation:
-            self.xml_element.set("translation", "{0:.6g} {1:.6g} {2:.6g}".format(*final_translation))
-        if self.ctx.export_rotation:
-            self.xml_element.set("rotation", " ".join(f"{math.degrees(a):.6g}" for a in final_rotation))
-        if self.ctx.export_scale:
-            self.xml_element.set("scale", "{0:.6g} {1:.6g} {2:.6g}".format(*final_scale))
-
-
-class Keyframes(BaseAnimationNode):
-    def __init__(self,
-                 i3d: I3D,
-                 fps: float,
-                 node: SceneGraphNode | SkinnedMeshBoneNode,
-                 channelbag: bpy.types.ActionChannelbag,
-                 start_frame: int):
-        super().__init__(i3d, fps, node)
-        self.channelbag = channelbag
-        self.start_frame = start_frame
-        self.fcurves = self._filter_fcurves(channelbag)
-        self.has_translation = any(fc.data_path.endswith("location") for fc in self.fcurves)
-        self.has_rotation = any(fc.data_path.endswith("rotation_euler") for fc in self.fcurves)
-        self.has_scale = any(fc.data_path.endswith("scale") for fc in self.fcurves)
-        self.should_bake = self.needs_baking(self.fcurves)
-
-        self.xml_element = self._generate_keyframes()
-
     @staticmethod
-    def needs_baking(fcurves: list[bpy.types.FCurve]) -> bool:
-        """Check if any fcurve requires baking. E.g. if it has animated constraints etc."""
-        return any(not fc.data_path.endswith((
-            "location", "rotation_euler", "rotation_quaternion", "scale")) for fc in fcurves
-        )
+    def _needs_baking(fcurves: list[bpy.types.FCurve]) -> bool:
+        """Returns True if *any* FCurve animates a non-transform property."""
+        valid_paths = ("location", "rotation_euler", "rotation_quaternion", "scale")
+        return any(not fc.data_path.endswith(path) for fc in fcurves for path in valid_paths if fc.data_path)
 
     @property
     def is_empty(self) -> bool:
@@ -123,26 +98,36 @@ class Keyframes(BaseAnimationNode):
 
     def _generate_keyframes(self) -> xml_i3d.Element:
         xml_element = xml_i3d.Element("Keyframes", {"nodeId": str(self.node.id)})
-        keyframe_list = sorted({kp.co.x for fc in self.fcurves for kp in fc.keyframe_points})
+
+        if self._needs_baking(self.fcurves):
+            # When baking, we need to use the start and end frame of the action
+            # and we will get object transforms for each frame between them.
+            keyframe_list = list(range(self.start_frame, self.end_frame + 1))
+            self.i3d.logger.debug(f"[{self.node.name}] Baking keyframes from {self.start_frame} to {self.end_frame}")
+            self.has_translation = True
+            self.has_rotation = True
+            self.has_scale = True
+        else:
+            keyframe_list = sorted({kp.co.x for fc in self.fcurves for kp in fc.keyframe_points})
 
         if not keyframe_list:
+            self.i3d.logger.debug(f"[{self.node.name}] No keyframes found")
             return xml_element
 
         for frame in keyframe_list:
             # Convert frame to time in milliseconds and ensure time always starts with 0ms
             time_ms = ((frame - self.start_frame) / self.fps) * 1000
-            context = KeyframeContext(
-                node=self.node,
-                is_bone=self.is_bone,
-                frame=frame,
-                time_ms=time_ms,
+            keyframe_element = Keyframe.generate_keyframe(
+                self.i3d,
+                self.node,
+                self.is_bone,
+                frame,
+                time_ms,
                 export_translation=self.has_translation,
                 export_rotation=self.has_rotation,
-                export_scale=self.has_scale,
-                should_bake=self.should_bake,
+                export_scale=self.has_scale
             )
-            keyframe = Keyframe(self.i3d, context)
-            xml_element.append(keyframe.xml_element)
+            xml_element.append(keyframe_element)
         return xml_element
 
 
@@ -176,15 +161,16 @@ class Clip(BaseAnimationExport):
             if node.blender_object.type == 'ARMATURE':
                 for bone in node.blender_object.data.bones:
                     if (bone_node := self.i3d.processed_objects.get(bone)):
-                        keyframes = Keyframes(self.i3d, self.fps, bone_node, channelbag, start_frame)
+                        keyframes = Keyframes(self.i3d, self.fps, bone_node, channelbag, start_frame, end_frame)
                         if not keyframes.is_empty:
                             self.xml_element.append(keyframes.xml_element)
                 continue  # skip processing the armature object itself
 
             self.i3d.logger.debug(f"[{node.name}] Processing completed")
 
-            keyframes = Keyframes(self.i3d, self.fps, node, channelbag, start_frame)
+            keyframes = Keyframes(self.i3d, self.fps, node, channelbag, start_frame, end_frame)
             if not keyframes.is_empty:
+                self.i3d.logger.debug(f"[{node.name}] Keyframes found")
                 self.xml_element.append(keyframes.xml_element)
 
         self.xml_element.set("duration", f"{duration_ms:.6g}")
