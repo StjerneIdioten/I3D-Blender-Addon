d7378ae55b9a46ec6581d7125cddb0c5bd3612ff
Kristian <57712777+NMC-TBone@users.noreply.github.com>
2025-11-11 13:34:10 +0100

    fix(user-attributes): enhance user attributes UI layout and interaction for better usability

--- addon/i3dio/ui/user_attributes.py
+++ addon/i3dio/ui/user_attributes.py
@@ -75,7 +75,15 @@ class I3DUserAttributes(bpy.types.PropertyGroup):
 class I3D_IO_UL_user_attributes(bpy.types.UIList):
     """UIList for i3d user attributes"""
     def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
-        layout.prop(item, 'name', text='', icon='SCRIPT', emboss=False, translate=False)
+        split = layout.split(factor=0.35, align=True)
+        split.prop(item, "name", text="", icon="SCRIPT", emboss=False)
+
+        sub = split.split(factor=0.5, align=True)
+        sub.prop(item, "type", text="")
+        if item.type == "data_boolean":
+            sub.prop(item, item.type, text="True" if item.data_boolean else "False", toggle=True)
+        else:
+            sub.prop(item, item.type, text="")
 
 
 @register
@@ -127,18 +135,11 @@ class I3D_IO_PT_user_attributes(Panel):
         attrs = context.object.i3d_user_attributes
 
         row = layout.row()
-        row.template_list("I3D_IO_UL_user_attributes", "The_List", attrs, "attribute_list", attrs, 'active_attribute')
+        row.template_list("I3D_IO_UL_user_attributes", "", attrs, "attribute_list", attrs, 'active_attribute')
 
         column = row.column(align=True)
-        column.operator('i3dio_user_attribute_list.new_item', text='', icon='ADD')
-        column.operator('i3dio_user_attribute_list.delete_item', text='', icon='REMOVE')
-
-        if 0 <= attrs.active_attribute < len(attrs.attribute_list):
-            attr = attrs.attribute_list[attrs.active_attribute]
-            row = layout.row()
-            row.alignment = 'RIGHT'
-            row.prop(attr, 'type')
-            row.prop(attr, attr.type, text='')
+        column.operator(I3D_IO_OT_new_user_attribute.bl_idname, text='', icon='ADD')
+        column.operator(I3D_IO_OT_delete_user_attribute.bl_idname, text='', icon='REMOVE')
 
 
 def register():
